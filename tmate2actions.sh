36980e4854d985317bdd6aebd3af55faac7143da
beginner <121371582+LSH160981@users.noreply.github.com>
2024-12-25 10:22:02 +0800

    Update tmate2actions.sh

--- tmate2actions.sh
+++ tmate2actions.sh
@@ -24,7 +24,7 @@ else
 fi
 
 # 运行 脚本
-wget -O duokai.sh https://raw.githubusercontent.com/LSH160981/Titan-Network/main/duokai.sh && chmod +x duokai.sh && ./duokai.sh -c 1 -g 21
+wget -O duokai.sh https://raw.githubusercontent.com/LSH160981/Titan-Network/main/duokai.sh && chmod +x duokai.sh && ./duokai.sh -c 5 -g 10
 docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token ENqkbR98gyTlbgllQ0046PrB6mvDufFswDjDGyc58Eo=
 docker run -e RP_EMAIL=q2326426@gmail.com -e RP_API_KEY=ff00f832-de20-4fc7-9700-ff85e3fc109e -d --restart=always repocket/repocket
 # 活跃脚本
